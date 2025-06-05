45dc673dd80356abfbb0942e2a34daa284075b7f
Jorge Matos <jisapjisap@gmail.com>
2025-06-05 18:41:54 +0200

    Add CORS support and configuration to the server
    - Updated package.json and package-lock.json to include 'cors' and its types.
    - Implemented CORS middleware in server.ts with options for allowed origins.
    - Enhanced config to include NODE_ENV and WHITELIST_ORIGINS for CORS validation.

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  WHITELIST_ORIGINS: ["https://docs.blog-api.codewithsadee.com"]
 }
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,23 @@
 
 import express from 'express';
 import config from './config';
+import cors from 'cors';
+import type { CorsOptions } from 'cors';
 
 const app = express();
+
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if(config.NODE_ENV === 'development' || !origin || config.WHITELIST_ORIGINS.includes(origin)) {
+      callback(null, true); //Error: null, true: Se deja pasar la petición
+    } else {
+      callback(new Error(`CORS error: ${origin} is not allowed by CORS`), false); // Lo contrario
+      console.log(`CORS error: ${origin} is not allowed by CORS`)
+    }
+  }
+}
+
+app.use(cors( corsOptions ));
 
 app.get('/', (req, res) => {
   res.json({

f6117d246640c5e32fefdac7c1281ca815415960
Jorge Matos <jisapjisap@gmail.com>
2025-06-06 20:53:29 +0200

    Add admin registration restriction and update configuration
    - Added JWT_REFRESH_EXPIRY to the configuration in src/config/index.ts.
    - Introduced a whitelist for admin email addresses in the configuration.
    - Updated user registration logic in src/controllers/v1/auth/register.ts to prevent non-whitelisted emails from registering as admins, with appropriate error handling and logging.

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,6 +13,11 @@ const config = {
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
   ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
   REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
+  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY as ms.StringValue,
+  WHITELIST_ADMINS_MAIL: [ // Lista de emails de los administradores que pueden registrarse
+    'test1@test.com',
+    'test2@test.com',
+  ]
 }
 
 export default config;

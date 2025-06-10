72f690ea3e7cbe7b4bae4bc0c631a0c8f3fe8365
Jorge Matos <jisapjisap@gmail.com>
2025-06-10 12:54:27 +0200

    Add update current user functionality and route
    - Introduced a new controller in src/controllers/v1/user/update_current_user.ts to handle updating the current user's information, including fields like username, email, and social links.
    - Updated the user routes in src/routes/v1/user.ts to include a new PUT endpoint for updating the current user, integrating authentication and authorization checks.
    - Added error handling for user not found and server errors in the update process.

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,4 +1,5 @@
 import getCurrentUser from "@/controllers/v1/user/get_current_user";
+import { updateCurrentUser } from "@/controllers/v1/user/update_current_user";
 import authenticate from "@/middlewares/authenticate";
 import authorize from "@/middlewares/authorize";
 import validationError from "@/middlewares/validationError";
@@ -15,6 +16,13 @@ router.get(
   authenticate, // header -> token -> verify -> userId -> req.userId -> next
   authorize(['admin', 'user']), // userId -> verificamos si existe en bd -> verificamos si el rol es admin o user -> next
   getCurrentUser
+)
+
+router.put(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  updateCurrentUser
 )
 
 

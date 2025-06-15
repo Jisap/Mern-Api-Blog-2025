e8bbbe1f216845eab46dae079e4570e18d426886
Jorge Matos <jisapjisap@gmail.com>
2025-06-15 20:03:43 +0200

    Add like routes to API
    - Introduced a new route for handling likes in the API by importing likeRoutes into the main router.
    - Updated the router configuration to include the '/likes' endpoint, enhancing the functionality of the application.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import authRoutes from '@/routes/v1/auth';
 import userRoutes from '@/routes/v1/user';
 import blogRoutes from '@/routes/v1/blog';
+import likeRoutes from '@/routes/v1/like';
 
 
 const router = Router();
@@ -20,5 +21,6 @@ router.get('/', (req, res) => {
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes); // localhost:3000/api/vi/users/current
 router.use('/blogs', blogRoutes);
+router.use('/likes', likeRoutes);
 
 export default router;
---
+++ src/routes/v1/like.ts
@@ -0,0 +1,10 @@
+
+
+import { Router } from "express";
+import authenticate from "@/middlewares/authenticate";
+import authorize from "@/middlewares/authorize";
+
+
+const router = Router();
+
+export default router;

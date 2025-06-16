93d40d813e30eb93abda7d61308e1bb7ebfe2846
Jorge Matos <jisapjisap@gmail.com>
2025-06-16 13:49:06 +0200

    Enhance blog like route with validation
    - Updated the POST endpoint for liking a blog to include validation for blogId and userId, ensuring proper data integrity.
    - Changed the route from '/blog/:blogId' to '/blogs/:blogId' for consistency with plural naming conventions.
    - Integrated express-validator and a middleware for handling validation errors, improving error handling in the application.

--- src/routes/v1/like.ts
+++ src/routes/v1/like.ts
@@ -4,14 +4,26 @@ import { Router } from "express";
 import authenticate from "@/middlewares/authenticate";
 import authorize from "@/middlewares/authorize";
 import likeBlog from "@/controllers/v1/like/likeBlog";
+import { body, param } from "express-validator";
+import validationError from "@/middlewares/validationError";
+
 
 
 const router = Router();
 
 router.post(
-  '/blog/:blogId',
+  '/blogs/:blogId',
   authenticate,
   authorize(['admin', 'user']),
+  param('blogId')
+    .isMongoId()
+    .withMessage('Invalid blog id'),
+  body('userId')
+    .notEmpty()
+    .withMessage('User id is required')
+    .isMongoId()
+    .withMessage('Invalid user id'),
+  validationError,
   likeBlog,
 )
 
